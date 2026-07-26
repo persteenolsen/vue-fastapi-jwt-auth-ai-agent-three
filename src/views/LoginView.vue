e4a55bf5c00503518b69f196cc2504c212d6126a
Per Olsen <persteen22@gmail.com>
2026-07-26 08:30:34 +0200

    Tested the build and deployment process

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -23,7 +23,7 @@ function onSubmit(values, { setErrors }) {
         <h3>AI Agent Chat Assistant with Tool Integration Using LangChain, FastAPI and Vue</h3>
         
         <br/><br />
-        Last updated: 24-07-2026<br /><br />
+        Last updated: 26-07-2026<br /><br />
         
         <h2>Login</h2>
         <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
